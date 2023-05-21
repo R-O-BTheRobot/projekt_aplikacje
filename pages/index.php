165f70445f59523ae3196f675a5e86bf27bf4953
Student <pzx104829@student.wsb.poznan.pl>
2023-05-21 19:25:51 +0200

    Swapping placeholder links to the ecommerce example

--- pages/index.php
+++ pages/index.php
@@ -25,22 +25,22 @@
             <div class="card-body">
               <div class="row">
                 <div class="col-sm-2">
-                  <a href="https://via.placeholder.com/1200/FFFFFF.png?text=1" data-toggle="lightbox" data-title="sample 1 - white" data-gallery="gallery">
+                  <a href="https://phpfinal.robtherobot.space/pages/examples/e-commerce.html" data-toggle="lightbox" data-title="sample 1 - white" data-gallery="gallery">
                     <img src="https://via.placeholder.com/300/FFFFFF?text=1" class="img-fluid mb-2" alt="white sample">
                   </a>
                 </div>
                 <div class="col-sm-2">
-                  <a href="https://via.placeholder.com/1200/000000.png?text=2" data-toggle="lightbox" data-title="sample 2 - black" data-gallery="gallery">
+                  <a href="https://phpfinal.robtherobot.space/pages/examples/e-commerce.html" data-toggle="lightbox" data-title="sample 2 - black" data-gallery="gallery">
                     <img src="https://via.placeholder.com/300/000000?text=2" class="img-fluid mb-2" alt="black sample">
                   </a>
                 </div>
                 <div class="col-sm-2">
-                  <a href="https://via.placeholder.com/1200/FF0000/FFFFFF.png?text=3" data-toggle="lightbox" data-title="sample 3 - red" data-gallery="gallery">
+                  <a href="https://phpfinal.robtherobot.space/pages/examples/e-commerce.html" data-toggle="lightbox" data-title="sample 3 - red" data-gallery="gallery">
                     <img src="https://via.placeholder.com/300/FF0000/FFFFFF?text=3" class="img-fluid mb-2" alt="red sample">
                   </a>
                 </div>
                 <div class="col-sm-2">
-                  <a href="https://via.placeholder.com/1200/FF0000/FFFFFF.png?text=4" data-toggle="lightbox" data-title="sample 4 - red" data-gallery="gallery">
+                  <a href="https://phpfinal.robtherobot.space/pages/examples/e-commerce.html" data-toggle="lightbox" data-title="sample 4 - red" data-gallery="gallery">
                     <img src="https://via.placeholder.com/300/FF0000/FFFFFF?text=4" class="img-fluid mb-2" alt="red sample">
                   </a>
                 </div>
