f5e8212910e7c53d54d9fd81f95e5951a8fc999a
Student <pzx104829@student.wsb.poznan.pl>
2023-05-21 19:52:35 +0200

    Fixing /scripts/dbconnect.php

--- scripts/dbconnect.php
+++ scripts/dbconnect.php
@@ -1,4 +1,4 @@
 <?php
-  $conn = mysqli_connect("localhost", "root", "admin", "sklep_xyz", "3306");
+  $conn = mysqli_connect("127.0.0.1", "root", "admin", "sklep_xyz", "3306");
   echo "db" . $conn->connect_errno;
 ?>
