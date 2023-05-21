d338bd09b54bdbc319ca2fdb6c72d37e43c40311
Dawid G <pzx104829@student.wsb.poznan.pl>
2023-05-21 20:35:25 +0200

    Adding a basic sidebar into /pages/index.php

--- pages/index.php
+++ pages/index.php
@@ -14,99 +14,105 @@
     <link rel="stylesheet" href="../dist/css/adminlte.min.css">
   </head>
   <body>
-  <section class="content">
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-12">
-          <div class="card card-primary">
-            <div class="card-header">
-              <h4 class="card-title">Witamy na stronie sklepu internetowego XYZ!</h4>
-            </div>
-            <div class="card-body">
-              <div class="row">
-                <?php
-                  require_once "../scripts/dbconnect.php";
-                  //echo $conn->connect_errno;
+  <div class="wrapper">
+    <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    </aside>
+    <div class="content-wrapper">
+      <section class="content">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-12">
+              <div class="card card-primary">
+                <div class="card-header">
+                  <h4 class="card-title">Witamy na stronie sklepu internetowego XYZ!</h4>
+                </div>
+                <div class="card-body">
+                  <div class="row">
+                    <?php
+                    require_once "../scripts/dbconnect.php";
+                    //echo $conn->connect_errno;
 
-                  $sql = "SELECT * FROM `products`";
-                  $result = $conn->query($sql);
-                  while ($product = $result->fetch_assoc())
-                  {
-                    echo <<< GET_PRODUCTS_FROM_DB
+                    $sql = "SELECT * FROM `products`";
+                    $result = $conn->query($sql);
+                    while ($product = $result->fetch_assoc())
+                    {
+                      echo <<< GET_PRODUCTS_FROM_DB
                     <div class="col-sm-2">
                         <a href="https://phpfinal.robtherobot.space/pages/examples/e-commerce.html?product=$product[product_id]" data-toggle="lightbox" data-title="$product[product_id]" data-gallery="gallery">
                                 <img src="$product[picture_link]" class="img-fluid mb-2" alt="Zdjęcie produktu $product[product_id]">
                         </a>
                     </div>
 GET_PRODUCTS_FROM_DB;
-                  }
-                ?>
-                <!--<div class="col-sm-2">
-                  <a href="https://phpfinal.robtherobot.space/pages/examples/e-commerce.html" data-toggle="lightbox" data-title="sample 1 - white" data-gallery="gallery">
-                    <img src="https://via.placeholder.com/300/FFFFFF?text=1" class="img-fluid mb-2" alt="white sample">
-                  </a>
+                    }
+                    ?>
+                    <!--<div class="col-sm-2">
+                      <a href="https://phpfinal.robtherobot.space/pages/examples/e-commerce.html" data-toggle="lightbox" data-title="sample 1 - white" data-gallery="gallery">
+                        <img src="https://via.placeholder.com/300/FFFFFF?text=1" class="img-fluid mb-2" alt="white sample">
+                      </a>
+                    </div>
+                    <div class="col-sm-2">
+                      <a href="https://phpfinal.robtherobot.space/pages/examples/e-commerce.html" data-toggle="lightbox" data-title="sample 2 - black" data-gallery="gallery">
+                        <img src="https://via.placeholder.com/300/000000?text=2" class="img-fluid mb-2" alt="black sample">
+                      </a>
+                    </div>
+                    <div class="col-sm-2">
+                      <a href="https://phpfinal.robtherobot.space/pages/examples/e-commerce.html" data-toggle="lightbox" data-title="sample 3 - red" data-gallery="gallery">
+                        <img src="https://via.placeholder.com/300/FF0000/FFFFFF?text=3" class="img-fluid mb-2" alt="red sample">
+                      </a>
+                    </div>
+                    <div class="col-sm-2">
+                      <a href="https://phpfinal.robtherobot.space/pages/examples/e-commerce.html" data-toggle="lightbox" data-title="sample 4 - red" data-gallery="gallery">
+                        <img src="https://via.placeholder.com/300/FF0000/FFFFFF?text=4" class="img-fluid mb-2" alt="red sample">
+                      </a>
+                    </div>
+                    <div class="col-sm-2">
+                      <a href="https://via.placeholder.com/1200/000000.png?text=5" data-toggle="lightbox" data-title="sample 5 - black" data-gallery="gallery">
+                        <img src="https://via.placeholder.com/300/000000?text=5" class="img-fluid mb-2" alt="black sample">
+                      </a>
+                    </div>
+                    <div class="col-sm-2">
+                      <a href="https://via.placeholder.com/1200/FFFFFF.png?text=6" data-toggle="lightbox" data-title="sample 6 - white" data-gallery="gallery">
+                        <img src="https://via.placeholder.com/300/FFFFFF?text=6" class="img-fluid mb-2" alt="white sample">
+                      </a>
+                    </div>
+                    <div class="col-sm-2">
+                      <a href="https://via.placeholder.com/1200/FFFFFF.png?text=7" data-toggle="lightbox" data-title="sample 7 - white" data-gallery="gallery">
+                        <img src="https://via.placeholder.com/300/FFFFFF?text=7" class="img-fluid mb-2" alt="white sample">
+                      </a>
+                    </div>
+                    <div class="col-sm-2">
+                      <a href="https://via.placeholder.com/1200/000000.png?text=8" data-toggle="lightbox" data-title="sample 8 - black" data-gallery="gallery">
+                        <img src="https://via.placeholder.com/300/000000?text=8" class="img-fluid mb-2" alt="black sample">
+                      </a>
+                    </div>
+                    <div class="col-sm-2">
+                      <a href="https://via.placeholder.com/1200/FF0000/FFFFFF.png?text=9" data-toggle="lightbox" data-title="sample 9 - red" data-gallery="gallery">
+                        <img src="https://via.placeholder.com/300/FF0000/FFFFFF?text=9" class="img-fluid mb-2" alt="red sample">
+                      </a>
+                    </div>
+                    <div class="col-sm-2">
+                      <a href="https://via.placeholder.com/1200/FFFFFF.png?text=10" data-toggle="lightbox" data-title="sample 10 - white" data-gallery="gallery">
+                        <img src="https://via.placeholder.com/300/FFFFFF?text=10" class="img-fluid mb-2" alt="white sample">
+                      </a>
+                    </div>
+                    <div class="col-sm-2">
+                      <a href="https://via.placeholder.com/1200/FFFFFF.png?text=11" data-toggle="lightbox" data-title="sample 11 - white" data-gallery="gallery">
+                        <img src="https://via.placeholder.com/300/FFFFFF?text=11" class="img-fluid mb-2" alt="white sample">
+                      </a>
+                    </div>
+                    <div class="col-sm-2">
+                      <a href="https://via.placeholder.com/1200/000000.png?text=12" data-toggle="lightbox" data-title="sample 12 - black" data-gallery="gallery">
+                        <img src="https://via.placeholder.com/300/000000?text=12" class="img-fluid mb-2" alt="black sample">
+                      </a>
+                    </div>-->
+                  </div>
                 </div>
-                <div class="col-sm-2">
-                  <a href="https://phpfinal.robtherobot.space/pages/examples/e-commerce.html" data-toggle="lightbox" data-title="sample 2 - black" data-gallery="gallery">
-                    <img src="https://via.placeholder.com/300/000000?text=2" class="img-fluid mb-2" alt="black sample">
-                  </a>
-                </div>
-                <div class="col-sm-2">
-                  <a href="https://phpfinal.robtherobot.space/pages/examples/e-commerce.html" data-toggle="lightbox" data-title="sample 3 - red" data-gallery="gallery">
-                    <img src="https://via.placeholder.com/300/FF0000/FFFFFF?text=3" class="img-fluid mb-2" alt="red sample">
-                  </a>
-                </div>
-                <div class="col-sm-2">
-                  <a href="https://phpfinal.robtherobot.space/pages/examples/e-commerce.html" data-toggle="lightbox" data-title="sample 4 - red" data-gallery="gallery">
-                    <img src="https://via.placeholder.com/300/FF0000/FFFFFF?text=4" class="img-fluid mb-2" alt="red sample">
-                  </a>
-                </div>
-                <div class="col-sm-2">
-                  <a href="https://via.placeholder.com/1200/000000.png?text=5" data-toggle="lightbox" data-title="sample 5 - black" data-gallery="gallery">
-                    <img src="https://via.placeholder.com/300/000000?text=5" class="img-fluid mb-2" alt="black sample">
-                  </a>
-                </div>
-                <div class="col-sm-2">
-                  <a href="https://via.placeholder.com/1200/FFFFFF.png?text=6" data-toggle="lightbox" data-title="sample 6 - white" data-gallery="gallery">
-                    <img src="https://via.placeholder.com/300/FFFFFF?text=6" class="img-fluid mb-2" alt="white sample">
-                  </a>
-                </div>
-                <div class="col-sm-2">
-                  <a href="https://via.placeholder.com/1200/FFFFFF.png?text=7" data-toggle="lightbox" data-title="sample 7 - white" data-gallery="gallery">
-                    <img src="https://via.placeholder.com/300/FFFFFF?text=7" class="img-fluid mb-2" alt="white sample">
-                  </a>
-                </div>
-                <div class="col-sm-2">
-                  <a href="https://via.placeholder.com/1200/000000.png?text=8" data-toggle="lightbox" data-title="sample 8 - black" data-gallery="gallery">
-                    <img src="https://via.placeholder.com/300/000000?text=8" class="img-fluid mb-2" alt="black sample">
-                  </a>
-                </div>
-                <div class="col-sm-2">
-                  <a href="https://via.placeholder.com/1200/FF0000/FFFFFF.png?text=9" data-toggle="lightbox" data-title="sample 9 - red" data-gallery="gallery">
-                    <img src="https://via.placeholder.com/300/FF0000/FFFFFF?text=9" class="img-fluid mb-2" alt="red sample">
-                  </a>
-                </div>
-                <div class="col-sm-2">
-                  <a href="https://via.placeholder.com/1200/FFFFFF.png?text=10" data-toggle="lightbox" data-title="sample 10 - white" data-gallery="gallery">
-                    <img src="https://via.placeholder.com/300/FFFFFF?text=10" class="img-fluid mb-2" alt="white sample">
-                  </a>
-                </div>
-                <div class="col-sm-2">
-                  <a href="https://via.placeholder.com/1200/FFFFFF.png?text=11" data-toggle="lightbox" data-title="sample 11 - white" data-gallery="gallery">
-                    <img src="https://via.placeholder.com/300/FFFFFF?text=11" class="img-fluid mb-2" alt="white sample">
-                  </a>
-                </div>
-                <div class="col-sm-2">
-                  <a href="https://via.placeholder.com/1200/000000.png?text=12" data-toggle="lightbox" data-title="sample 12 - black" data-gallery="gallery">
-                    <img src="https://via.placeholder.com/300/000000?text=12" class="img-fluid mb-2" alt="black sample">
-                  </a>
-                </div>-->
               </div>
             </div>
           </div>
-        </div>
-      </div>
-    </div><!-- /.container-fluid -->
-  </section>
+        </div><!-- /.container-fluid -->
+      </section>
+    </div>
+  </div>
   </body>
 </html>
