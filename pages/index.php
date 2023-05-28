da93d525430807cba563e4b06f31cc95cce7860e
Student <pzx104829@student.wsb.poznan.pl>
2023-05-28 18:48:36 +0200

    Centering the page list of index.php

--- pages/index.php
+++ pages/index.php
@@ -22,42 +22,7 @@
   <h1 style="align-center">Sklep z obuwiem</h1>
 </div>
 <div class="wrapper">
-  <div class="content-wrapper">
-    <section class="content">
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-12">
-            <div class="card card-primary">
-              <!-- <div class="card-header">
-                 <h4 class="card-title">Witamy na stronie sklepu internetowego XYZ!</h4>
-               </div> -->
-              <div class="card-body">
-                <div class="row">
-                  <?php
-                  require_once "../scripts/dbconnect.php";
-
-                  $sql = "SELECT * FROM `products`";
-                  $result = $conn->query($sql);
-                  while ($product = $result->fetch_assoc())
-                  {
-                    echo <<< GET_PRODUCTS_FROM_DB
-                    <div class="col-sm-2">
-                        <a href="https://phpfinal.robtherobot.space/pages/product.php?product=$product[product_id]" data-toggle="lightbox" data-title="$product[product_id]" data-gallery="gallery">
-                                <img src="$product[picture_link]" class="img-fluid mb-2" alt="Zdjęcie produktu $product[product_id]">
-                        </a>
-                    </div>
-GET_PRODUCTS_FROM_DB;
-                  }
-                  ?>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  </div>
-  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+  <aside class="main-sidebar sidebar-dark-primary elevation-4 d-flex align-items-stretch">
     <nav class="d-flex align-items-center">
       <ul class="nav nav-pills nav-sidebar flex-column vertical" data-widget="treeview" role="menu" data-accordion="false">
         <li class="nav-item">
@@ -98,6 +63,41 @@ GET_PRODUCTS_FROM_DB;
       </ul>
     </nav>
   </aside>
+  <div class="content-wrapper">
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-12">
+            <div class="card card-primary">
+              <!-- <div class="card-header">
+                 <h4 class="card-title">Witamy na stronie sklepu internetowego XYZ!</h4>
+               </div> -->
+              <div class="card-body">
+                <div class="row">
+                  <?php
+                  require_once "../scripts/dbconnect.php";
+
+                  $sql = "SELECT * FROM `products`";
+                  $result = $conn->query($sql);
+                  while ($product = $result->fetch_assoc())
+                  {
+                    echo <<< GET_PRODUCTS_FROM_DB
+                    <div class="col-sm-2">
+                        <a href="https://phpfinal.robtherobot.space/pages/product.php?product=$product[product_id]" data-toggle="lightbox" data-title="$product[product_id]" data-gallery="gallery">
+                                <img src="$product[picture_link]" class="img-fluid mb-2" alt="Zdjęcie produktu $product[product_id]">
+                        </a>
+                    </div>
+GET_PRODUCTS_FROM_DB;
+                  }
+                  ?>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </div>
 </body>
 </html>
